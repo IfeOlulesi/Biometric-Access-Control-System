175ed117167b881daf775327c1ae465c7e11eeca
Ife Olulesi <53560106+IfeOlulesi@users.noreply.github.com>
2021-04-30 22:40:11 +0100

    Setting up bootstrap

--- AccessControlSystem/settings.py
+++ AccessControlSystem/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "bs"),
 )
 
 TEMPLATE_DIRS = (
--- AccessControlSystem/urls.py
+++ AccessControlSystem/urls.py
@@ -19,13 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('acs.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [path('admin/', admin.site.urls), path('', include('acs.urls')),]
 
+# added the static dir
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# added the media directory for dev only
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root = settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
